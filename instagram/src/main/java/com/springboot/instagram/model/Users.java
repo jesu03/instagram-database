b2e9567f658bd5e093dc415185f355446e909a6d
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-08-14 17:08:44 +0530

    update users entity class

--- instagram/src/main/java/com/springboot/instagram/model/Users.java
+++ instagram/src/main/java/com/springboot/instagram/model/Users.java
@@ -1,8 +1,11 @@
 package com.springboot.instagram.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class Users {
+    @Id
+    private String id;
     
 }
