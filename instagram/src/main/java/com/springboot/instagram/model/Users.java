7c7e9e41b50d62dd1ea6632615ea412adf9905b3
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-08-24 13:32:06 +0530

    commit

--- instagram/src/main/java/com/springboot/instagram/model/Users.java
+++ instagram/src/main/java/com/springboot/instagram/model/Users.java
@@ -1,11 +1,6 @@
 package com.springboot.instagram.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 
-@Entity
 public class Users {
-    @Id
-    private String id;
     
 }
