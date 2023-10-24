77079dc7120c302c48a7d70dbcbeaff995940a39
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-10-24 13:14:03 +0530

    commit userService class

--- instagram/src/main/java/com/springboot/instagram/controller/UserController.java
+++ instagram/src/main/java/com/springboot/instagram/controller/UserController.java
@@ -2,6 +2,7 @@ package com.springboot.instagram.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,45 +14,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.instagram.model.User;
-import com.springboot.instagram.repository.UserRepository;
+import com.springboot.instagram.services.UserService;
 
 
 @RestController
 @RequestMapping("/api/users")
-public class UserController {
+@CrossOrigin("*")
+public class UserController{
+
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
     
     @GetMapping(value="/")
     public List<User> getAll(){
-        return userRepository.findAll();
+        return userService.findAll();
     }
 
     @PostMapping("/post")
     public List<User> persist(@RequestBody User user){
-         userRepository.save(user);
-         return userRepository.findAll();
+        userService.save(user);
+         return userService.findAll();
     }
     
 
     @GetMapping(value="/{userid}")
     public User get(@RequestParam("userid") String userid){
-            return userRepository.findById(userid).get();
+            return userService.findById(userid).get();
     }
 
     @PutMapping(value="/{userid}")
     public List<User> put(@PathVariable String userid,@RequestBody User user){
-        if(userRepository.existsById(userid)){
-            userRepository.deleteById(userid);
-            userRepository.save(user);
+        if(userService.existsById(userid)){
+            userService.deleteById(userid);
+            userService.save(user);
         }
-        return userRepository.findAll();
+        return userService.findAll();
     }
 
     @DeleteMapping(value="/{userid}")
     public List<User> delete(@PathVariable String userid){
-        userRepository.deleteById(userid);
-        return userRepository.findAll();
+        userService.deleteById(userid);
+        return userService.findAll();
     }
  
 }
---
+++ instagram/src/main/java/com/springboot/instagram/services/UserService.java
@@ -0,0 +1,228 @@
+package com.springboot.instagram.services;
+
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
+import org.springframework.stereotype.Service;
+
+import com.springboot.instagram.model.User;
+import com.springboot.instagram.repository.UserRepository;
+
+@Service
+public class UserService implements UserRepository{
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        return userRepository.findById(id);
+    }
+
+
+    @Override
+    public void deleteById(String id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public boolean existsById(String id) {
+        return userRepository.existsById(id);
+    }
+
+    public User updateById(String userid,User user){
+        return userRepository.save(user);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void flush() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'flush'");
+    }
+
+    @Override
+    public <S extends User> S saveAndFlush(S entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveAndFlush'");
+    }
+
+    @Override
+    public <S extends User> List<S> saveAllAndFlush(Iterable<S> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveAllAndFlush'");
+    }
+
+    @Override
+    public void deleteAllInBatch(Iterable<User> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAllInBatch'");
+    }
+
+    @Override
+    public void deleteAllByIdInBatch(Iterable<String> ids) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAllByIdInBatch'");
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAllInBatch'");
+    }
+
+    @Override
+    public User getOne(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getOne'");
+    }
+
+    @Override
+    public User getById(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+    @Override
+    public User getReferenceById(String id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getReferenceById'");
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public <S extends User> List<S> findAll(Example<S> example, Sort sort) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public <S extends User> List<S> saveAll(Iterable<S> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'saveAll'");
+    }
+
+    
+
+    @Override
+    public List<User> findAllById(Iterable<String> ids) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAllById'");
+    }
+
+    
+
+    
+
+    
+
+    @Override
+    public long count() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+
+    
+
+    @Override
+    public void delete(User entity) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> ids) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAllById'");
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends User> entities) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAll'");
+    }
+
+    @Override
+    public void deleteAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAll'");
+    }
+
+    @Override
+    public List<User> findAll(Sort sort) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Page<User> findAll(Pageable pageable) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public <S extends User> Optional<S> findOne(Example<S> example) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findOne'");
+    }
+
+    @Override
+    public <S extends User> Page<S> findAll(Example<S> example, Pageable pageable) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public <S extends User> long count(Example<S> example) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'count'");
+    }
+
+    @Override
+    public <S extends User> boolean exists(Example<S> example) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'exists'");
+    }
+
+    @Override
+    public <S extends User, R> R findBy(Example<S> example, Function<FetchableFluentQuery<S>, R> queryFunction) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findBy'");
+    }
+    
+}
